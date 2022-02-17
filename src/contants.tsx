2f11fcde3f398665e095b6b0b8cc53e1f38aa648
tufailmirr <tufailmirr@gmail.com>
2022-02-17 13:06:20 +0530

    add contant file for storing local usable data

---
+++ src/contants.tsx
@@ -0,0 +1,81 @@
+export const sortData = [
+  {
+    value: "",
+    name: "All",
+  },
+  {
+    value: "1 - Jerry",
+    name: "Jerry",
+  },
+  {
+    value: "2 - Elaine",
+    name: "Elaine",
+  },
+  {
+    value: "3 - Kramer",
+    name: "Kramer",
+  },
+  {
+    value: "4 - George",
+    name: "George",
+  },
+];
+
+export const categoryData = [
+  {
+    name: "Software Development",
+    value: "software-dev",
+  },
+  {
+    name: "Customer Service",
+    value: "customer-support",
+  },
+  {
+    name: "Design",
+    value: "design",
+  },
+  {
+    name: "Marketing",
+    value: "marketing",
+  },
+  {
+    name: "Sales",
+    value: "sales",
+  },
+  {
+    name: "Product",
+    value: "product",
+  },
+  {
+    name: "Business",
+    value: "business",
+  },
+  {
+    name: "Data",
+    value: "data",
+  },
+  {
+    name: "DevOps / Sysadmin",
+    value: "devops",
+  },
+  {
+    name: "Finance / Legal",
+    value: "finance-legal",
+  },
+  {
+    name: "Human Resources",
+    value: "hr",
+  },
+  {
+    name: "QA",
+    value: "qa",
+  },
+  {
+    name: "Writing",
+    value: "writing",
+  },
+  {
+    name: "All others",
+    value: "all-others",
+  },
+];
